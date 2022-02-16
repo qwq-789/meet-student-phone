4960fd2abbed51d602a9fafc5ee2ed821d93878a
qwq-789 <elfrain520@gmail.com>
2022-02-17 00:51:07 +0800

    v2

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
-import './style.css'
+import "./style.css";
+
+var burgerButton = document.getElementById("bb");
+var burger = document.getElementById("burger");
+
+burgerButton.addEventListener("click", function () {
+  burger.classList.toggle("-translate-y-72");
+  burger.classList.toggle("translate-y-10");
+});
